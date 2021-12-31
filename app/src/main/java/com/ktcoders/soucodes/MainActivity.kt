6ad23d4eaa827e70769a6ae2820873de414b3cac
mehdi <asoodeh.dev@gmail.com>
2022-01-01 00:03:47 +0330

    init

--- app/src/main/java/com/ktcoders/soucodes/MainActivity.kt
+++ app/src/main/java/com/ktcoders/soucodes/MainActivity.kt
@@ -2,15 +2,14 @@ package com.ktcoders.soucodes
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.ktcoders.utils.MyExtensions.toast
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-//        this.toast("hi dude")
-
-
+        this.toast("hi dude")
 
     }
 }
